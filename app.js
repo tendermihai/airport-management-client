d95efe547e13bb3c009be716c9f30e2b0ea81e54
Tender <tender121995@gmail.com>
2023-04-07 16:38:35 +0300

    created-add-post-functionality

--- app.js
+++ app.js
@@ -1,43 +1,23 @@
-async function getAirports() {
-  let data = await fetch("http://localhost:8080/api/v1/airports/all");
-  let conv = await data.json();
-  console.log(conv);
-  attachCards(conv);
+let selectBtn = document.querySelector(".sort");
+
+if (selectBtn) {
+  selectBtn.addEventListener("change", async () => {
+    console.log(selectBtn.value);
+
+    let data = await getSortedBy(selectBtn.value);
+    attachCards(data);
+  });
+
+  home();
 }
+let submitBtn = document.querySelector(".submitAirport");
 
-getAirports();
-
-function createCard(airport) {
-  let section = document.createElement("section");
-
-  section.innerHTML = `
-  
-  <div class="card mb-3" style="max-width: 540px;">
-  <div class="row g-0">
-      <div class="col-md-8">
-          <div class="card-body">
-              <h5 class="card-title">${airport.name}</h5>
-              <p class="id">ID: ${airport.id}</p>
-              <p class="name">Name: ${airport.name}</p>
-              <p class="code">Code: ${airport.code}</p>
-              <p class="municipality">Municipality: ${airport.municipality}</p>
-              <p class="elevation">Elevation: ${airport.elevation}</p>
-              <p class="region-code">Region-code: ${airport.region_code}</p>
-          </div>
-      </div>
-  </div>
-</div>
-  
-  `;
-
-  return section;
-}
-
-function attachCards(airports) {
-  let container = document.querySelector(".container");
-  container.innerHTML = "";
-
-  airports.forEach((airport) => {
-    container.appendChild(createCard(airport));
+if (submitBtn) {
+  submitBtn.addEventListener("click", () => {
+    const name = document.querySelector(".name").value;
+    const code = document.querySelector(".code").value;
+    const municipality = document.querySelector(".municipality").value;
+    const region_code = document.querySelector(".regionCode").value;
+    postAirport({ name, code, municipality, region_code }).value;
   });
 }
